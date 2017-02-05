31f1a5a3951a135dab17caf65ea4009a5e1b7419
Frederick Burkley <burkley@gmail.com>
2017-02-04 20:26:07 -0500

    Creating a directory (to write the file to) if the directory does not exist.

--- src/main/java/org/fgb/io/filecache/impl/FileSizeFileGenerator.java
+++ src/main/java/org/fgb/io/filecache/impl/FileSizeFileGenerator.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 import org.fgb.io.filecache.FileGenerator;
 import org.fgb.io.filecache.util.RandomLineHelper;
 
-public class FileSizeFileGenerator  implements FileGenerator {
+public class FileSizeFileGenerator implements FileGenerator {
 
 	/**
 	 * The name of this class.
@@ -24,6 +24,10 @@ public class FileSizeFileGenerator  implements FileGenerator {
 	 */
 	private final String fileName;
 	/**
+	 * The name of the directory where the file will be generated.
+	 */
+	private final String dirName;
+	/**
 	 * The size of the file that will be generated
 	 */
 	private final long fileSize;
@@ -31,15 +35,26 @@ public class FileSizeFileGenerator  implements FileGenerator {
 	 * 
 	 */
 	private final RandomLineHelper randomLineHelper;
-	
-	
+
 	public FileSizeFileGenerator(final String fileName, long fileSize, final short numberOfFieldsPerLine) {
 		super();
-		System.out.println(_className);
+//		System.out.println(_className + ".FileSizeFileGenerator: Constructor...");
+
 		this.fileName = fileName;
+		int index = this.fileName.lastIndexOf(File.separator);
+		if (index >= 0) {
+		this.dirName = this.fileName.substring(0, index);
+		} else {
+			this.dirName = "." + File.separator;
+		}
+		
 		this.fileSize = fileSize;
 		this.randomLineHelper = new RandomLineHelper(numberOfFieldsPerLine);
+//		System.out.println(_className + ".FileSizeFileGenerator: fileName = " + this.fileName);
+//		System.out.println(_className + ".FileSizeFileGenerator: fileName.lastIndexOf(\"/\") = " + this.fileName.lastIndexOf(File.separator));
+//		System.out.println(_className + ".FileSizeFileGenerator: dirName = " + this.dirName);
 	}
+
 
 	@Override
 	public void generateFile() throws IOException {
@@ -47,14 +62,23 @@ public class FileSizeFileGenerator  implements FileGenerator {
 		byte[] bytesToWrite;
 		long bytesWritten = 0;
 		long startTime = System.currentTimeMillis();
+		File dir = new File(this.dirName);
+		if (!dir.exists()) {
+			try {
+				dir.mkdirs();
+			} catch (SecurityException ex) {
+				throw new IOException(ex);
+			}
+		}
 		File file = new File(this.fileName);
-		
+
 		try (FileOutputStream outputStream = new FileOutputStream(file)) {
 			do {
 				bytesToWrite = this.randomLineHelper.getLine().getBytes();
-//				msg.append(_className).append(".generateFile(): Wrote ").append(bytesToWrite.length).append(" bytes to file.");
-//				System.out.println(msg.toString());
-//				msg.delete(0, msg.length());
+				// msg.append(_className).append(".generateFile(): Wrote
+				// ").append(bytesToWrite.length).append(" bytes to file.");
+				// System.out.println(msg.toString());
+				// msg.delete(0, msg.length());
 				outputStream.write(bytesToWrite);
 				bytesWritten += bytesToWrite.length;
 			} while (bytesWritten < this.fileSize);
@@ -65,19 +89,19 @@ public class FileSizeFileGenerator  implements FileGenerator {
 	@Override
 	public void setBenchmark(boolean benchmark) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void enableZipCompression(boolean compress) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void enableGZipCompression(boolean compress) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
